4c2f27c6f14527fa117c52c57d27bb195ab7a98f
chunza2542 <chunza2542@hotmail.com>
2018-07-19 20:28:15 +0700

    cleanup file directory

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+// @chunza2542
+// prettier code format configuration file
+
+module.exports = {
+  parser: "flow",
+  tabWidth: 2,
+  useTabs: true,
+  semi: false,
+  singleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  arrowParens: "always",
+}
